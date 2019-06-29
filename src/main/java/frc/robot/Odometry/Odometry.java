72c379d42c562a665f965a6619bbd034c36d26e1
infinity1096 <2315465052@qq.com>
2019-06-29 13:17:49 +0800

    DCMotorTransmission class added

---
+++ src/main/java/frc/robot/Odometry/Odometry.java
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Odometry;
+
+/**
+ * Add your docs here.
+ */
+public class Odometry implements Runnable {
+
+    double[][] a(){
+        double[][] b = {{1,0},{0,1}};
+        b[1][0]
+        return b;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
