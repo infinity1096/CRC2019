63ab07d595316c0d34edfdd8545affbddfa6dae4
Ava-OvO <48372856+Ava-OvO@users.noreply.github.com>
2019-07-24 15:42:20 +0800

    shoot

---
+++ src/main/java/frc/robot/commands/Intake/Shoot.java
@@ -0,0 +1,61 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.Intake;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.Intake;
+
+public class Shoot extends Command {
+
+  Timer timer = new Timer();
+  
+  public Shoot() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.intake);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    timer.reset();
+    Robot.intake.intakeDown();
+    
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.intake.intakeClose();
+    timer.start();
+    Robot.intake.shoot();
+    if(timer.get()>0.3){
+      Robot.intake.IntakeOpen();
+      Robot.intake.hold();
+      
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
